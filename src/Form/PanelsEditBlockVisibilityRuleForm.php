a0cf0b0f844ba64e38548488515d5e06d9ff5db5
vbouchet31 <vincent.bouchet.31@gmail.com>
2017-04-14 12:40:48 +0200

    Initiate the form to configure the block visibility rule.

---
+++ src/Form/PanelsEditBlockVisibilityRuleForm.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @file
+ */
+
+namespace Drupal\panels\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a form for editing a block visibility rule.
+ */
+class PanelsEditBlockVisibilityRuleForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'panels_edit_block_visibility_rule_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // TODO: Implement buildForm() method.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // TODO: Implement submitForm() method.
+  }
+}

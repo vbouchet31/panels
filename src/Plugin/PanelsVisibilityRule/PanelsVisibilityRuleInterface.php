97ca4e7639188dbde54fcd0fc4673bce3692d0f8
vbouchet31 <vincent.bouchet.31@gmail.com>
2017-04-14 12:12:46 +0200

    Initiate the interface to be used by the visibility rules plugins.

---
+++ src/Plugin/PanelsVisibilityRule/PanelsVisibilityRuleInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\panels;
+
+use Drupal\Component\Plugin\ConfigurablePluginInterface;
+use Drupal\Component\Plugin\PluginInspectionInterface;
+use Drupal\Core\Plugin\PluginFormInterface;
+
+/**
+ * Provides an interface for a Panels Visibility Rule.
+ */
+interface PanelsVisibilityRuleInterface extends PluginInspectionInterface, ConfigurablePluginInterface, PluginFormInterface {
+
+  /**
+   * Check if the block is visible.
+   *
+   * @return bool
+   *   TRUE if the widget is visible.
+   */
+  public function checkBlockVisibility();
+
+  // @TODO: We can probably add a similar feature to regions.
+
+}
